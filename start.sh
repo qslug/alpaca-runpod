58593588aa68286de3a3ee79863d6b57861e9c51
Augusto de la Torre <augustod@spotify.com>
2023-04-15 15:20:16 +0200

    Set proper Jupyter preferred_dir

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ if [[ $JUPYTER_PASSWORD ]]
 then
     jupyter nbextension enable --py widgetsnbextension
     jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace/EveryDream2trainer
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace/
 else
     echo "Container Started"
     sleep infinity
